93bbe18a0f9a74d89b5baf289438c24e7e6536fe
JordanEckford <jordan.eckford@gmail.com>
2024-01-09 14:03:08 +0000

    added localStorage functionality to save results after page refresh

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const playerOneHeader = document.getElementById("player-1");
 const playerTwoHeader = document.getElementById("player-2");
 const playerOneScore = document.getElementById("player-1-score");
 const playerTwoScore = document.getElementById("player-2-score");
-const restartButton = document.getElementById("restart-game");
+const changeNameButton = document.getElementById("change-name");
 const blackBall = document.getElementById("black");
 const pinkBall = document.getElementById("pink");
 const blueBall = document.getElementById("blue");
@@ -12,7 +12,6 @@ const greenBall = document.getElementById("green");
 const yellowBall = document.getElementById("yellow");
 const redBall = document.getElementById("red");
 const balls = document.getElementById("balls");
-const turnSwitcher = document.getElementById("turn-switcher");
 const player1Tile = document.getElementById("player-1-tile");
 const player2Tile = document.getElementById("player-2-tile");
 const bulk = document.getElementById("bulk");
@@ -22,16 +21,76 @@ const safety = document.getElementById("safety");
 const miss = document.getElementById("miss");
 const addRed = document.getElementById("add-red");
 const minusRed = document.getElementById("minus-red");
+const timerContainer = document.getElementById("timer-container");
+const minsElapsed = document.getElementById("mins-elapsed");
+const secsElapsed = document.getElementById("secs-elapsed");
+const endFrameButton = document.getElementById("end-frame");
+const playerOneFrameCountContainer = document.getElementById("player-1-framecount");
+const playerTwoFrameCountContainer = document.getElementById("player-2-framecount");
 
 let playerOne = "";
 let playerTwo = "";
 let playerOneTotal = 0;
 let playerTwoTotal = 0;
+let playerOneFrameCount;
+let playerTwoFrameCount;
+
 let totalRedsLeft = 15;
 let currentFoul = false;
+let timerSeconds = 0;
+let timerMinutes = 0;
+let currentBreak = 0;
+let frameNumber = 1;
+let gamesPlayed;
+let started = false;
+
+if (window.localStorage.gamesPlayed) {
+ gamesPlayed = window.localStorage.gamesPlayed;
+} else {
+ gamesPlayed = 0;
+ window.localStorage.gamesPlayed = 0;
+}
+if (window.localStorage.playerOneFrameCount) {
+ playerOneFrameCount = window.localStorage.playerOneFrameCount;
+ playerOneFrameCountContainer.innerHTML = playerOneFrameCount;
+} else {
+ playerOneFrameCount = 0;
+ window.localStorage.playerOneFrameCount = 0;
+ playerOneFrameCountContainer.innerHTML = playerOneFrameCount;
+}
+if (window.localStorage.playerTwoFrameCount) {
+ playerTwoFrameCount = window.localStorage.playerTwoFrameCount;
+ playerTwoFrameCountContainer.innerHTML = playerTwoFrameCount;
+} else {
+ playerTwoFrameCount = 0;
+ window.localStorage.playerTwoFrameCount = 0;
+ playerTwoFrameCountContainer.innerHTML = playerTwoFrameCount;
+}
+
+const timer = setInterval(() => {
+ if (started) {
+  if (timerSeconds === 59) {
+   timerMinutes++;
+   minsElapsed.innerHTML = timerMinutes + ":";
+   timerSeconds = 0;
+   secsElapsed.innerHTML = "0" + timerSeconds;
+  } else {
+   timerSeconds++;
+   secsElapsed.innerHTML = timerSeconds < 10 ? "0" + timerSeconds : timerSeconds;
+  }
+ }
+}, 1000);
+
+let currentPots = {
+ 1: { red: 0, yellow: 0, green: 0, brown: 0, blue: 0, pink: 0, black: 0, score: 0 },
+ 2: { red: 0, yellow: 0, green: 0, brown: 0, blue: 0, pink: 0, black: 0, score: 0 },
+};
 
 const undo = [];
 const redo = [];
+
+// window.localStorage.test = JSON.stringify({ red: 1, blue: 8 });
+// console.log(JSON.parse(window.localStorage.test));
 
 const ballsArray = [blackBall, pinkBall, blueBall, brownBall, greenBall, yellowBall];
 
@@ -54,6 +113,27 @@ disableColours(ballsArray);
 
 let currentPlayer = 1;
 
+player1Tile.addEventListener("click", () => {
+ currentPlayer = 1;
+ player2Tile.className = "";
+ player1Tile.className = "current";
+});
+player2Tile.addEventListener("click", () => {
+ currentPlayer = 2;
+ player1Tile.className = "";
+ player2Tile.className = "current";
+});
+
+if (window.localStorage.player1 && window.localStorage.player2) {
+ playerOne = window.localStorage.player1;
+ playerTwo = window.localStorage.player2;
+ nameForm.className = "hidden";
+ bulk.className = "";
+ changeNameButton.className = "";
+ playerOneHeader.innerHTML = playerOne;
+ playerTwoHeader.innerHTML = playerTwo;
+ started = true;
+}
 function changePlayer() {
  if (currentPlayer === 1) {
   currentPlayer = 2;
@@ -65,20 +145,6 @@ function changePlayer() {
   player1Tile.className = "current";
  }
 }
-
-turnSwitcher.addEventListener("click", () => {
- changePlayer();
-});
-
-if (window.localStorage.player1 && window.localStorage.player2) {
- playerOne = window.localStorage.player1;
- playerTwo = window.localStorage.player2;
- nameForm.className = "hidden";
- restartButton.className = "";
- playerOneHeader.innerHTML = playerOne;
- playerTwoHeader.innerHTML = playerTwo;
-}
-
 nameForm.addEventListener("submit", (event) => {
  event.preventDefault();
  playerOne = event.target[0].value;
@@ -86,15 +152,32 @@ nameForm.addEventListener("submit", (event) => {
  nameForm.className = "hidden";
  playerOneHeader.innerHTML = playerOne;
  playerTwoHeader.innerHTML = playerTwo;
- restartButton.className = "";
+ changeNameButton.className = "";
  window.localStorage.player1 = playerOne;
  window.localStorage.player2 = playerTwo;
  bulk.className = "";
+ started = true;
 });
 
-restartButton.addEventListener("click", () => {
- nameForm.className = "";
- bulk.className = "hidden";
+changeNameButton.addEventListener("click", () => {
+ const check = confirm("This will delete all game data!");
+ if (check) {
+  localStorage.clear();
+  nameForm.className = "";
+  bulk.className = "hidden";
+  changeNameButton.className = "hidden";
+  //**************************************************************************************** */
+  playerOneFrameCount = 0;
+  playerTwoFrameCount = 0;
+  playerOneTotal = 0;
+  playerTwoTotal = 0;
+  playerOneFrameCountContainer.innerHTML = playerOneFrameCount;
+  playerTwoFrameCountContainer.innerHTML = playerTwoFrameCount;
+  playerOneScore.innerHTML = playerOneTotal;
+  playerTwoScore.innerHTML = playerOneTotal;
+ } else {
+  return;
+ }
 });
 
 blackBall.addEventListener("click", () => {
@@ -112,6 +195,8 @@ blackBall.addEventListener("click", () => {
   changePlayer();
   return;
  }
+ currentPots[currentPlayer].black++;
+ window.localStorage[frameNumber] = JSON.stringify(currentPots);
  if (currentPlayer === 1) {
   playerOneTotal += 7;
   playerOneScore.innerHTML = playerOneTotal;
@@ -136,6 +221,8 @@ pinkBall.addEventListener("click", () => {
   changePlayer();
   return;
  }
+ currentPots[currentPlayer].pink++;
+ window.localStorage[frameNumber] = JSON.stringify(currentPots);
  if (currentPlayer === 1) {
   playerOneTotal += 6;
   playerOneScore.innerHTML = playerOneTotal;
@@ -160,6 +247,8 @@ blueBall.addEventListener("click", () => {
   changePlayer();
   return;
  }
+ currentPots[currentPlayer].blue++;
+ window.localStorage[frameNumber] = JSON.stringify(currentPots);
  if (currentPlayer === 1) {
   playerOneTotal += 5;
   playerOneScore.innerHTML = playerOneTotal;
@@ -184,6 +273,8 @@ brownBall.addEventListener("click", () => {
   changePlayer();
   return;
  }
+ currentPots[currentPlayer].brown++;
+ window.localStorage[frameNumber] = JSON.stringify(currentPots);
  if (currentPlayer === 1) {
   playerOneTotal += 4;
   playerOneScore.innerHTML = playerOneTotal;
@@ -208,6 +299,8 @@ greenBall.addEventListener("click", () => {
   changePlayer();
   return;
  }
+ currentPots[currentPlayer].green++;
+ window.localStorage[frameNumber] = JSON.stringify(currentPots);
  if (currentPlayer === 1) {
   playerOneTotal += 3;
   playerOneScore.innerHTML = playerOneTotal;
@@ -232,6 +325,8 @@ yellowBall.addEventListener("click", () => {
   changePlayer();
   return;
  }
+ currentPots[currentPlayer].yellow++;
+ window.localStorage[frameNumber] = JSON.stringify(currentPots);
  if (currentPlayer === 1) {
   playerOneTotal += 2;
   playerOneScore.innerHTML = playerOneTotal;
@@ -257,6 +352,8 @@ redBall.addEventListener("click", () => {
   return;
  }
  if (totalRedsLeft > 0) {
+  currentPots[currentPlayer].red++;
+  window.localStorage[frameNumber] = JSON.stringify(currentPots);
   enableColours(ballsArray);
   if (currentPlayer === 1) {
    playerOneTotal += 1;
@@ -274,6 +371,7 @@ redBall.addEventListener("click", () => {
 foulMSG.addEventListener("click", () => {
  currentFoul = false;
  foulMSG.className = "hidden";
+ disableColours(ballsArray);
 });
 foul.addEventListener("click", () => {
  currentFoul = true;
@@ -298,3 +396,40 @@ minusRed.addEventListener("click", () => {
   redBall.innerHTML = totalRedsLeft;
  }
 });
+endFrameButton.addEventListener("click", () => {
+ const check = confirm("Confirm end game, progress will be stored");
+ if (check) {
+  currentPots[1].score = playerOneTotal;
+  currentPots[2].score = playerTwoTotal;
+  window.localStorage[frameNumber] = JSON.stringify(currentPots);
+  if (playerOneTotal === playerTwoTotal) return alert("settle draw!");
+  if (playerOneTotal > playerTwoTotal) {
+   playerOneFrameCount++;
+   playerOneFrameCountContainer.innerHTML = playerOneFrameCount;
+   window.localStorage.playerOneFrameCount = playerOneFrameCount;
+  } else {
+   playerTwoFrameCount++;
+   playerTwoFrameCountContainer.innerHTML = playerTwoFrameCount;
+   window.localStorage.playerTwoFrameCount = playerTwoFrameCount;
+  }
+  frameNumber++;
+  currentPots = {
+   1: { red: 0, yellow: 0, green: 0, brown: 0, blue: 0, pink: 0, black: 0, score: 0 },
+   2: { red: 0, yellow: 0, green: 0, brown: 0, blue: 0, pink: 0, black: 0, score: 0 },
+  };
+  totalRedsLeft = 15;
+  redBall.innerHTML = totalRedsLeft;
+  gamesPlayed++;
+  window.localStorage.gamesPlayed = gamesPlayed;
+  timerMinutes = 0 + ":";
+  timerSeconds = 0;
+  playerOneTotal = 0;
+  playerTwoTotal = 0;
+  playerOneScore.innerHTML = playerOneTotal;
+  playerTwoScore.innerHTML = playerTwoTotal;
+ }
+});
+
+// const test = document.createElement("p");
+// test.innerHTML = window.localStorage.player2;
+// document.getElementById("results").appendChild(test);
